b6f24f3b0ec65574d427611b33e0ff849ebaebd2
OhYoon Kwon <rdd2033@naver.com>
2025-01-26 19:56:45 +0900

    [Gold III] Title: 개구리 점프, Time: 1396 ms, Memory: 165256 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/17619. 개구리 점프/개구리 점프.java
+++ 백준/Gold/17619. 개구리 점프/개구리 점프.java
@@ -1,97 +1,108 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
 /*
 1. x1 좌표가 작은것 기준으로 정렬
-2. 맨 앞 통나무 무터 다음 통나무를 보면서 최대한 갈 수 있는 좌표 기록
-3. 만약 다음으로 못가면 그 다음 통나무 부터 탐색하되, 시작지점은 이전 통나무의 최대 지점
-4. 주의할거는 이전 통나무 최대 지점이 다음것보다 작은 경우, Q의 좌표 순서가 이상한 경우(순서반대)
+2. 정렬된 통나무를 2개씩 보면서 이동할 수 있으면 union
  */
 
 public class Main {
     static int N, Q;
     static PriorityQueue<Log> pq;
-    static List<Log> sortedlist, list;
-    static HashMap<Integer, Integer> map;
+    static int[] parent, rank;
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         N = Integer.parseInt(st.nextToken());
         Q = Integer.parseInt(st.nextToken());
         pq = new PriorityQueue<>(Comparator.comparing(Log::getStart).thenComparing(Log::getEnd));
-        sortedlist = new ArrayList<>(N);
-        list = new ArrayList<>(N);
-        map = new HashMap<>();
+        parent = new int[N + 1];
+        rank = new int[N + 1];
+        init();
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            Log log = new Log(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
-            pq.add(log);
-            list.add(log);
-            sortedlist.add(log);
-            st.nextToken();
+            pq.add(new Log(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), i + 1));
         }
 
-        sortedlist.sort(Comparator.comparing(Log::getStart).thenComparing(Log::getEnd));
-        pq.poll();
-        int maxEnd = -1;
-        for (int i = 0; i < N; i++) {
-            Log now = sortedlist.get(i);
-            while (!pq.isEmpty()) {
-                Log next = pq.peek();
-                if (now.getEnd() >= next.getStart()) {
-                    maxEnd = next.getEnd();
-                    pq.poll();
-                } else {
-                    break;
-                }
+        Log now = pq.poll();
+        while (!pq.isEmpty()) {
+            Log next = pq.poll();
+            if (now.getEnd() >= next.getStart()) {
+                union(now.idx, next.idx);
             }
 
-            map.put(now.getStart(), maxEnd);
+            if(now.getEnd() < next.getEnd()){
+                now = next;
+            }
         }
 
         for (int i = 0; i < Q; i++) {
             st = new StringTokenizer(br.readLine());
-            int from = Integer.parseInt(st.nextToken()) - 1;
-            int to = Integer.parseInt(st.nextToken()) - 1;
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
 
-            Log fromLog = list.get(from);
-            Log toLog = list.get(to);
+            int p1 = find(from);
+            int p2 = find(to);
 
-            if (fromLog.getStart() > toLog.getStart()) {
-                Log temp = fromLog;
-                fromLog = toLog;
-                toLog = temp;
-            }
-
-            if (map.get(fromLog.getStart()) >= toLog.getStart()) {
+            if (p1 == p2)
                 System.out.println(1);
-            } else {
+            else
                 System.out.println(0);
-            }
         }
-
 
         br.close();
     }
 
-    static class Log {
-        int start;
-        int end;
+    static void init() {
+        for (int i = 1; i <= N; i++) {
+            parent[i] = i;
+        }
+    }
 
-        public Log(int start, int end) {
+    static int find(int node) {
+        if (parent[node] == node) {
+            return node;
+        }
+
+        return parent[node] = find(parent[node]);
+    }
+
+    static void union(int n1, int n2) {
+        int p1 = find(n1);
+        int p2 = find(n2);
+
+        if (rank[p1] > rank[p2]) {
+            parent[n2] = p1;
+        } else {
+            parent[n1] = p2;
+        }
+
+        if (rank[p1] == rank[p2]) {
+            rank[p2]++;
+        }
+    }
+
+    static class Log {
+        int start, end;
+        int idx;
+
+        public Log(int start, int end, int idx) {
             this.start = start;
             this.end = end;
+            this.idx = idx;
         }
 
         public int getStart() {
-            return start;
+            return this.start;
         }
 
         public int getEnd() {
-            return end;
+            return this.end;
         }
     }
 }
