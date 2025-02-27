e64fdda3eafc2a7047242af8c4113d30d0982dc3
OhYoon Kwon <rdd2033@naver.com>
2025-02-27 15:09:57 +0900

    [level 2] Title: 구명보트, Time: 11.14 ms, Memory: 58.2 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -1,35 +1,26 @@
 import java.util.*;
 
-/*
-1. 정렬 후 투포인터로 가장 가벼운거랑 무거운거 태움
-2. 투포인터가 만날 때까지 진행하며 못태운거는 혼자 타라해
-*/
-
 class Solution {
     public int solution(int[] people, int limit) {
-        int answer = 0;
-        Arrays.sort(people);
+        int ans = 0;
         int s = 0;
         int e = people.length - 1;
+        Arrays.sort(people);
         
-        
-        // 3개일 경우는?
-        while(s <= e){
-            int sum = people[s] + people[e];
-            if(sum <= limit){
+        while(s < e){
+            if(people[s] + people[e] <= limit){
                 s++;
                 e--;
             } else {
                 e--;
             }
-            answer++;
+            ans++;
         }
         
-        return answer;
+        if(s == e){
+            ans++;
+        }
+        
+        return ans;
     }
 }
-
-    
-    
-    
-    
