2cde1efd10ba567f04fff14cf71b8351a324eb65
Ohyoon Kwon <rdd2033@naver.com>
2025-03-01 19:40:59 +0900

    [Gold II] Title: 컬러볼, Time: 1036 ms, Memory: 125940 KB -BaekjoonHub

--- 백준/Gold/10800. 컬러볼/컬러볼.java
+++ 백준/Gold/10800. 컬러볼/컬러볼.java
@@ -6,74 +6,59 @@ import java.util.*;
 public class Main {
     static int N;
     static List<Ball> balls;
-    static int[][] accum;
+    static int[] accum;
     static StringBuilder sb;
+    static int[] ans;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        balls = new LinkedList<>();
-        accum = new int[N + 1][2];
+        balls = new ArrayList<>();
+        accum = new int[N + 1];
+        ans = new int[N];
         sb = new StringBuilder();
 
         StringTokenizer st;
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            balls.add(new Ball(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), i, 0));
+            balls.add(new Ball(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), i));
         }
 
-        balls.sort(Comparator.comparing(Ball::getSize).thenComparing(Ball::getColor));
+        balls.sort(Comparator.comparing(Ball::getSize));
 
-        Ball prev = new Ball(-1, -1, -1, -1);
         int sum = 0;
-        int prevSum = 0;
+        int lastIdx = 0;
         for (Ball b : balls) {
-
-            if (prev.size != b.size) {
-                b.ans = sum - accum[b.color][0];
-                prevSum = sum;
-                accum[b.color][1] = accum[b.color][0];
-                
-            } else {
-                if (prev.color != b.color) {
-                    accum[b.color][1] = accum[b.color][0];
-                }
-                b.ans = prevSum - accum[b.color][1];
+            while (b.size > balls.get(lastIdx).size) {
+                Ball lastBall = balls.get(lastIdx);
+                sum += lastBall.size;
+                accum[lastBall.color] += lastBall.size;
+                lastIdx++;
             }
 
-            prev = b;
-            accum[b.color][0] += b.size;
-            sum += b.size;
+            ans[b.originIdx] = sum - accum[b.color];
         }
-        balls.sort(Comparator.comparing(Ball::getOriginIdx));
-        for (Ball b : balls) {
-            sb.append(b.ans).append("\n");
-        }
-        System.out.println(sb);
 
+        for (int a : ans) {
+            sb.append(a).append("\n");
+        }
+
+        System.out.println(sb);
         br.close();
     }
 
     static class Ball {
-        int color, size, originIdx, ans;
+        int color, size, originIdx;
 
-        public Ball(int color, int size, int originIdx, int ans) {
+        public Ball(int color, int size, int originIdx) {
             this.color = color;
             this.size = size;
             this.originIdx = originIdx;
-            this.ans = ans;
-        }
-
-        int getColor() {
-            return color;
         }
 
         int getSize() {
             return size;
         }
 
-        int getOriginIdx() {
-            return originIdx;
-        }
     }
 }
